f43b43310ba62c56a12901d4bc3f67e5dd2bc6ed
Timothy Barmann <tbarmann92@gmail.com>
2016-03-31 14:09:52 -0400

    first commit

---
+++ src/main.js
@@ -0,0 +1,11 @@
+// src/main.js
+import React, { Component } from 'react';
+import { render } from 'react-dom';
+
+class Root extends Component {
+  render() {
+    return <h1> Hello World </h1>;
+  }
+}
+
+render(<Root />, document.getElementById('root'));
